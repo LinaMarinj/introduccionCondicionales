f50aaf46d325d1a99e3ddfc9149bef6c9d79d8e8
lina marin <linamarin2011@gmail.com>
2024-08-20 08:15:08 -0500

    ejercicio condicionales

---
+++ script.js
@@ -0,0 +1,13 @@
+
+let edad =18
+
+if(edad>18){
+    console.log("Es mayor de edad :)")
+} else if (edad == 18){
+    console.log("Apenas cumplio la mayoria de edad :)")
+}else if(edad < 18){
+    console.log("Es menor de edad :(")
+}else{
+    console.log("Ultima ejecucuión")
+
+}
